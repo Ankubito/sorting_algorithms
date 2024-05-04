a53666fc55538689eac51f3dc95c06d25b5a9ea4
Alphonse <prudent.nkubito@gmail.com>
2024-05-04 16:30:28 -0700

    Sorting Algorthm and Big O

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include <stdlib.h>
 
 /**
  * fetch_max - find maximum value in array of integers.
@@ -11,15 +10,15 @@
  */
 int fetch_max(int *array, int size)
 {
-    int maximum, b;
+	int maximum, b;
 
-    for (maximum = array[0], b = 1; b < size; b++)
-    {
-        if (array[b] > maximum)
-            maximum = array[b];
-    }
+	for (maximum = array[0], b = 1; b < size; b++)
+	{
+		if (array[b] > maximum)
+			maximum = array[b];
+	}
 
-    return maximum;
+	return (maximum);
 }
 
 /**
@@ -32,39 +31,39 @@ int fetch_max(int *array, int size)
  */
 void counting_sort(int *array, size_t size)
 {
-    int *countin, *sorted, maximum, a;
+	int *countin, *sorted, maximum, a;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    sorted = (int *)malloc(sizeof(int) * size);
-    if (sorted == NULL)
-        return;
-    maximum = fetch_max(array, size);
-    countin = (int *)malloc(sizeof(int) * (maximum + 1));
-    if (countin == NULL)
-    {
-        free(sorted);
-        return;
-    }
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+		return;
+	maximum = fetch_max(array, size);
+	countin = malloc(sizeof(int) * (maximum + 1));
+	if (countin == NULL)
+	{
+		free(sorted);
+		return;
+	}
 
-    for (a = 0; a < (maximum + 1); a++)
-        countin[a] = 0;
-    for (a = 0; a < (int)size; a++)
-        countin[array[a]] += 1;
-    for (a = 1; a < (maximum + 1); a++)
-        countin[a] += countin[a - 1];
-    print_array(countin, maximum + 1);
+	for (a = 0; a < (maximum + 1); a++)
+		countin[a] = 0;
+	for (a = 0; a < (int)size; a++)
+		countin[array[a]] += 1;
+	for (a = 0; a < (maximum + 1); a++)
+		countin[a] += countin[a - 1];
+	print_array(countin, maximum + 1);
 
-    for (a = (int)size - 1; a >= 0; a--)
-    {
-        sorted[countin[array[a]] - 1] = array[a];
-        countin[array[a]] -= 1;
-    }
+	for (a = 0; a < (int)size; a++)
+	{
+		sorted[countin[array[a]] - 1] = array[a];
+		countin[array[a]] -= 1;
+	}
 
-    for (a = 0; a < (int)size; a++)
-        array[a] = sorted[a];
+	for (a = 0; a < (int)size; a++)
+		array[a] = sorted[a];
 
-    free(sorted);
-    free(countin);
+	free(sorted);
+	free(countin);
 }
